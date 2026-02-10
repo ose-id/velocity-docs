4cf11ec2ee5a11574dcad0d2f5ddae60f4e4bfa6
adydetra <devdewa123.sp@gmail.com>
2026-02-10 16:54:28 +0700

    chore: return to top if swap page

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,14 @@ const router = createRouter({
       component: () => import('../views/TermsView.vue'),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    else {
+      return { top: 0 };
+    }
+  },
 });
 
 export default router;
